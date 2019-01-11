5c17a1f401c5eeed6486508e055fd539418b277b
Taric GANDI <taric.gandi@dauphine.eu>
2019-01-11 02:41:54 +0100

    Adding classes Person and PersonDao

---
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/dao/PersonDao.java
@@ -0,0 +1,96 @@
+package io.github.oliviercailloux.y2018.opendata.dao;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import io.github.oliviercailloux.y2018.opendata.entity.Person;
+
+/**
+ * Represents class which will help to manipulate Person Objet
+ * <br />
+ * Every method rejects null argument.
+ * @author Dauphine - GANDI Taric
+ */
+public class PersonDao extends AbstractDao<Person> {
+
+	/**
+	 * Inheritance of constructor from super class AbstractDao
+	 * 
+	 * @param entityManager
+	 * @param entityClass
+	 * @param entityName
+	 */
+	public PersonDao(EntityManager entityManager, Class<Person> entityClass, String entityName) {
+		super(entityManager, entityClass, entityName);
+	}
+
+	/**
+	 * This method help to find all records of person saved in DB
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Person> findAll() {
+		
+		 Query query = entityManager.createQuery("SELECT e FROM Person e");
+	     return query.getResultList();
+	}
+	
+	/**
+	 * Help to remove record of person with given id 
+	 */
+	@Override
+	public void remove(Long id) throws EntityDoesNotExistDaoException {
+		
+		 Query query = entityManager.createNativeQuery("DELETE FROM Person WHERE ID = " + id);
+		 query.executeUpdate();
+	}
+	/**
+	 * Help to find a record with given id
+	 */
+	@Override
+	public Optional<Person> findOne(Long id) {
+		
+	    Optional<Person> person = Optional.of((entityManager.find(Person.class, id)));
+		return person;
+	}
+	
+    /**
+     * Help to save an record of person	
+     */
+	@Override
+	public Person persist(Person entity) throws EntityAlreadyExistsDaoException {
+		
+		executeInsideTransaction(entityManager -> entityManager.persist(entity));
+		return entity;
+	}
+    /**
+     * help in merging an object person given person as param
+     */
+	@Override
+	public Person merge(Person entity) {
+		
+		executeInsideTransaction(entityManager -> entityManager.merge(entity));
+		flush();
+		return entity;
+	}
+
+	/**
+	 * Help to handle safely save of records and handling exception by using rollback
+	 * @param action
+	 */
+	private void executeInsideTransaction(Consumer<EntityManager> action) {
+		
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+        	transaction.begin();
+            action.accept(entityManager);
+            transaction.commit(); 
+        }
+        catch (RuntimeException exception) {
+        	transaction.rollback();
+            throw exception;
+        }
+    }
+}
---
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Person.java
@@ -0,0 +1,238 @@
+package io.github.oliviercailloux.y2018.opendata.entity;
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import com.google.common.base.Strings;
+
+/**
+ * Provides a basic implementation of class Person.<br />
+ * Person is either Student or Teacher. In this class you have basic attributes to
+ * manipulate class Person and getters an setters
+ * <br />
+ * @author Gandi Taric
+ */
+
+@Entity
+public class Person extends AbstractEntity {
+
+	private static final long serialVersionUID = -4768931293875213592L;
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+
+		LastName = lastName;
+	}
+
+	public String getAccountType() {
+
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+
+		this.accountType = accountType;
+
+	}
+
+	public List<String> getMemberShip() {
+
+		return memberShip;
+
+	}
+
+	public void setMemberShip(List<String> memberShip) {
+
+		this.memberShip = memberShip;
+
+	}
+
+	public String getDauphineMail() {
+
+		return dauphineMail;
+
+	}
+
+	public void setDauphineMail(String dauphineMail) {
+
+		this.dauphineMail = dauphineMail;
+
+	}
+
+	public String getOffice() {
+
+		return office;
+	}
+
+	/**
+	 * @param office if this parameter is null, it will be convert to an empty
+	 *               string
+	 */
+	public void setOffice(String office) {
+
+		this.office = Strings.nullToEmpty(office);
+	}
+
+	/**
+	 * 
+	 * @return a list of services accesses of the person
+	 */
+	public List<String> getAccessServices() {
+
+		return Collections.unmodifiableList(accessServices);
+	}
+
+	public void setAccessServices(String service) {
+
+		this.accessServices.add(service);
+	}
+
+	public String getPhoneNumer() {
+
+		return phoneNumer;
+	}
+
+	/**
+	 * @param phoneNumer if this parameter is null, it will be remplaced by an empty
+	 *                   string
+	 */
+	public void setPhoneNumer(String phoneNumer) {
+
+		this.phoneNumer = Strings.nullToEmpty(phoneNumer);
+	}
+
+	public String getTraining() {
+
+		return training;
+	}
+
+	/**
+	 * 
+	 * @param training will remplaced by empty string if it's null
+	 */
+	public void setTraining(String training) {
+
+		this.training = Strings.nullToEmpty(training);
+
+	}
+
+	public String getFax() {
+
+		return fax;
+	}
+
+	public void setFax(String fax) {
+
+		this.fax = Strings.nullToEmpty(fax);
+	}
+
+	public String getPersonnalMail() {
+
+		return personnalMail;
+	}
+
+	public void setPersonnalMail(String personnalMail) {
+
+		this.personnalMail = Strings.nullToEmpty(personnalMail);
+	}
+
+	public String getIne() {
+
+		return ine;
+	}
+
+	public void setIne(String ine) {
+
+		this.ine = Strings.nullToEmpty(ine);
+	}
+
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	private String firstName;
+
+	@NotNull
+	private String LastName;
+
+	@NotNull
+	private String accountType;
+
+	@NotNull
+	private List<String> memberShip;
+
+	@NotNull
+	private String dauphineMail;
+
+	private String office;
+	/**
+	 * This represent the list of services allowed to the student 
+	 */
+	private List<String> accessServices;
+
+	private String phoneNumer;
+	
+	/**
+	 * this field represent the training of the student and will 
+	 * be set to empty string if it is a teacher
+	 */
+	private String training;
+	
+    /**
+     * This represent fax of the person
+     */
+
+	private String fax;
+	
+	/**
+	 * this field represent the personnal mail of the person 
+	 */
+
+	private String personnalMail;
+	
+	/**
+	 * This field represent National Student code in case person is a student
+	 * it will be filled with empty string if person is a teacher
+	 */
+
+	private String ine;
+	
+	/**
+	 * This field help to check if person object is active or not
+	 */
+
+	private boolean isActive;
+
+}
