e359c6c7e0d69a317736e787d8e245d0ed9a4170
Diallo Elhadj Oumar <Omardjalo@omardjalo.local>
2019-01-12 12:06:38 +0100

    Update files

--- dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Entity.java
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Entity.java
@@ -15,4 +15,5 @@ public interface Entity extends Serializable {
 
 	Long getId();
 
+
 }
---
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Lecture.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.y2018.opendata.entity;
+
+public class Lecture extends AbstractEntity{
+
+	/**
+	 * Implemented by another member of the group
+	 */
+	private static final long serialVersionUID = 2283030879727615179L;
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
