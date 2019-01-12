9d9618cb36ebd4fe5bff67f13a93ea03628c1c24
Diallo Elhadj Oumar <Omardjalo@omardjalo.local>
2019-01-12 12:37:36 +0100

    Update files

---
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Person.java
@@ -0,0 +1,16 @@
+package io.github.oliviercailloux.y2018.opendata.entity;
+
+public class Person extends AbstractEntity {
+
+	/**
+	 * Implemented by another member of the group
+	 */
+	private static final long serialVersionUID = -5865100813791955358L;
+
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
