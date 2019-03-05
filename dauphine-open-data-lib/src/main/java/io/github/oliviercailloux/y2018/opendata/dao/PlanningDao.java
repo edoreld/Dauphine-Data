828287e038fe19ef7e9e5dadbf7e03088cf34f99
omardjalo <omardjalo@yahoo.fr>
2019-03-05 14:01:53 +0100

    Update class Planning and planningDao

--- dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/dao/PlanningDao.java
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/dao/PlanningDao.java
@@ -2,9 +2,7 @@ package io.github.oliviercailloux.y2018.opendata.dao;
 
 import java.util.List;
 import java.util.Optional;
-
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import io.github.oliviercailloux.y2018.opendata.entity.Planning;
 
@@ -29,11 +27,9 @@ public class PlanningDao extends AbstractDao<Planning>  {
 	 * The method allows to find the schedules recorded in the database
 	 */
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<Planning> findAll() {
-		Query query = entityManager.createQuery("SELECT e FROM " + entityClass.getSimpleName() + " e" );
-		return query.getResultList();
+		return entityManager.createQuery("SELECT e FROM " + entityClass.getSimpleName() + " e", entityClass).getResultList();
 	}
 
 	/**
@@ -51,7 +47,7 @@ public class PlanningDao extends AbstractDao<Planning>  {
 	 */
 
 	@Override
-	public Planning persist(Planning entity) {
+	public Planning persist(Planning entity) throws EntityAlreadyExistsDaoException {
 		entityManager.persist(entity);	
 		return entity;
 	}
@@ -70,7 +66,7 @@ public class PlanningDao extends AbstractDao<Planning>  {
 	 */
 
 	@Override
-	public void remove(Long id){
+	public void remove(Long id) throws EntityDoesNotExistDaoException {
 		Planning planning = entityManager.find(entityClass, id);
 		if(planning != null) 
 			entityManager.remove(planning);	
