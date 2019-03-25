2bcfe46f769466fa39f0ec36b807a2827a699868
dbarthe <barthelemy.delemotte@gmail.com>
2019-03-25 18:56:52 +0100

    fix a merge issue

--- dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Person.java
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/entity/Person.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.github.oliviercailloux.y2018.opendata.entity;
 
 import java.util.ArrayList;
@@ -263,32 +262,4 @@ public class Person implements io.github.oliviercailloux.y2018.opendata.entity.E
 	}
 
 }
-=======
-package io.github.oliviercailloux.y2018.opendata.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Setter
-@Getter
-@ToString
-@EqualsAndHashCode
-@NoArgsConstructor
-@AllArgsConstructor
-public class Person {
-
-	@Id
-	@GeneratedValue
-	private Long id;
-
-}
->>>>>>> partial correction
