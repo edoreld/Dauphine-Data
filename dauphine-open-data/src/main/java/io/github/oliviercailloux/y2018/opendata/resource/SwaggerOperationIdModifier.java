73c7b56c0ab8218c90ada6818f4456e07ced6725
dbarthe <barthelemy.delemotte@gmail.com>
2019-03-26 16:28:47 +0100

    improve swagger api definition with a Custom ReaderListener
    (cherry picked from commit 23d8a9a)

---
+++ dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/SwaggerOperationIdModifier.java
@@ -0,0 +1,76 @@
+package io.github.oliviercailloux.y2018.opendata.resource;
+
+import io.swagger.v3.jaxrs2.Reader;
+import io.swagger.v3.jaxrs2.ReaderListener;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * Swagger Custom modifier that changes the naming scheme of route.
+ * <p>
+ * This was necessary because swagger annotations didn't work well with our AbstractResource model and ended up
+ * giving the same OperationId to every routes sharing the same method.
+ * <p>
+ * This modifier replace the operation id like this :
+ * - POST /course -> create_course
+ * - PUT /course/{id} -> replace_course
+ * - GET /course -> list_courses
+ * ...
+ */
+@OpenAPIDefinition
+public class SwaggerOperationIdModifier implements ReaderListener {
+    
+    private final Pattern patternRouteId = Pattern.compile("/(.*)/\\{id\\}");
+    private final Pattern patternRouteNoId = Pattern.compile("/(.*)");
+    
+    private final List<String> ignoredRoutes = Arrays.asList("/home");
+    
+    @Override
+    public void beforeScan(Reader reader, OpenAPI openAPI) {
+    }
+    
+    @Override
+    public void afterScan(Reader reader, OpenAPI openAPI) {
+        openAPI.getPaths().forEach((path, pathItem) -> {
+            
+            if (ignoredRoutes.contains(path)) return ;
+            
+            Matcher matcherId = patternRouteId.matcher(path);
+            Matcher matcherNoId = patternRouteNoId.matcher(path);
+            
+            pathItem.readOperationsMap().forEach((httpMethod, operation) -> {
+                
+                if (matcherId.matches()) {
+    
+                    String action = httpMethod.name().toLowerCase();
+                    if (httpMethod.equals(PathItem.HttpMethod.PUT)) {
+                        action = "replace";
+                    }
+    
+                    operation.setOperationId(action + "_" + matcherId.group(1));
+                    
+                } else if (matcherNoId.matches()) {
+                    
+                    String action = httpMethod.name().toLowerCase();
+                    boolean plural = false;
+                    if (httpMethod.equals(PathItem.HttpMethod.GET)) {
+                        action = "list";
+                        plural = true;
+                    }
+                    else if (httpMethod.equals(PathItem.HttpMethod.POST)) {
+                        action = "create";
+                    }
+                    
+                    operation.setOperationId(action + "_" + matcherNoId.group(1) + (plural ? "s" : ""));
+                }
+            });
+        });
+    }
+}
