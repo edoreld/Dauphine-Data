cb302cfc2908eaa2da9a318ad0edb20e84e5e555
Francisco Javier Martinez Lago <edoreld@gmail.com>
2019-04-02 19:49:21 +0200

    first attempt search course by different criteria

--- dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/dao/CourseDao.java
+++ dauphine-open-data-lib/src/main/java/io/github/oliviercailloux/y2018/opendata/dao/CourseDao.java
@@ -1,6 +1,13 @@
 package io.github.oliviercailloux.y2018.opendata.dao;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import io.github.oliviercailloux.y2018.opendata.entity.Course;
 
@@ -9,6 +16,31 @@ public class CourseDao extends AbstractDao<Course> {
 
 	public CourseDao() {
 		super("Course", Course.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Course> findByNameDescLang(String name, String desc, String lang) {
+		if (name.isEmpty() && desc.isEmpty() && lang.isEmpty()) {
+			return Collections.EMPTY_LIST;
+		}
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Course> q = cb.createQuery(Course.class);
+		Root<Course> c = q.from(Course.class); // ~eq to FROM
+
+		if (!name.isEmpty()) {
+			q.where(cb.equal(c.get("courseName"), name));
+		}
+		if (!name.isEmpty()) {
+			q.where(cb.equal(c.get("courseDescription"), name));
+		}
+		if (!name.isEmpty()) {
+			q.where(cb.equal(c.get("instructionLanguage"), name));
+		}
+
+		TypedQuery<Course> query = em.createQuery(q);
+		return query.getResultList();
 	}
 
 }
--- dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/CourseResource.java
+++ dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/CourseResource.java
@@ -1,6 +1,17 @@
 package io.github.oliviercailloux.y2018.opendata.resource;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.github.oliviercailloux.y2018.opendata.dao.CourseDao;
 import io.github.oliviercailloux.y2018.opendata.entity.Course;
@@ -8,8 +19,30 @@ import io.github.oliviercailloux.y2018.opendata.entity.Course;
 @Path("course")
 public class CourseResource extends AbstractResource<Course, CourseDao> {
 
-	public CourseResource() {
-		super("Course", "course");
+	private static final Logger LOGGER = LoggerFactory.getLogger(CourseResource.class);
+
+	@PersistenceContext
+	protected EntityManager em;
+
+	public CourseResource(String resourceName, String resourcePath) {
+		super(resourceName, resourcePath);
+		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Filters a user query by name, desc or lang depending on the existing query parameters
+	 * @param lang {@link Course#instructionLanguage}
+	 * @param desc {@link Course#courseDescription}
+	 * @param name {@link Course#courseName}
+	 * @return list of {@link Course} entities that match the query parameters
+	 */
+	@GET
+	public Response filter(@DefaultValue("") @QueryParam("name") String name,
+			@DefaultValue("") @QueryParam("desc") String desc, @DefaultValue("") @QueryParam("lang") String lang) {
+
+		LOGGER.info("[{}] - finding entities by name [{}], description [{}] and language [{}] ..", resourceName, name,
+				desc, lang);
+		List<Course> courses = dao.findByNameDescLang(name, desc, lang);
+		return Response.ok(courses).build();
+	}
 }
