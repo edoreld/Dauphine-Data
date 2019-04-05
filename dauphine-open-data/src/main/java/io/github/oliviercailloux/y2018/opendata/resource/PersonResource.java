e65fa46a64c5ffb32ddf1dc74cd5767468812395
omardjalo <omardjalo@yahoo.fr>
2019-04-05 17:07:45 +0200

    update person resource

--- dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/PersonResource.java
+++ dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/PersonResource.java
@@ -1,9 +1,13 @@
 package io.github.oliviercailloux.y2018.opendata.resource;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import ezvcard.VCard;
+import ezvcard.Ezvcard;
+
 import io.github.oliviercailloux.y2018.opendata.dao.EntityAlreadyExistsDaoException;
 import io.github.oliviercailloux.y2018.opendata.dao.PersonDao;
 import io.github.oliviercailloux.y2018.opendata.entity.Person;
@@ -18,9 +22,12 @@ public class PersonResource extends AbstractResource<Person, PersonDao> {
 	
 	@POST
 	@Path("importvcard")
-	public void importVCard(VCard vcard ) throws EntityAlreadyExistsDaoException {
+	@Consumes("text/plain")
+	public Response importVCard(String vCardString) throws EntityAlreadyExistsDaoException {
+				
+		VCard vcard = Ezvcard.parse(vCardString).first();
 		VcardImport vcardImport = new VcardImport();
-
-		this.dao.persist(vcardImport.VcardToPerson(vcard));	
+		this.dao.persist(vcardImport.VcardToPerson(vcard));
+		return Response.ok("ok").build();
 	}
 }
