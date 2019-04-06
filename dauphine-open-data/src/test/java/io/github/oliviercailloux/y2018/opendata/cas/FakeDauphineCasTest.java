2bdaf09789f4434d6e46e25e88e4e69b82c0a338
dbarthe <barthelemy.delemotte@gmail.com>
2019-04-06 17:48:03 +0200

    code reformating

--- dauphine-open-data/src/test/java/io/github/oliviercailloux/y2018/opendata/cas/FakeDauphineCasTest.java
+++ dauphine-open-data/src/test/java/io/github/oliviercailloux/y2018/opendata/cas/FakeDauphineCasTest.java
@@ -14,39 +14,39 @@ import static org.mockito.BDDMockito.then;
 
 /**
  * This unit test class ensure the FakeDauphineCas is working as expected.
- *
+ * <p>
  * FakeDauphineCas is populated with a fake user list. We expect the user "loic" exists and has password "123" and role
  * "student"
  */
 public class FakeDauphineCasTest {
-	
-	private FakeDauphineCas cas;
-	
-	@Before
-	public void setup() {
-		cas = new FakeDauphineCas();
-	}
-	
-	@Test
-	public void testSequence() throws DauphineCasException {
-		String token = cas.authenticate(new Credentials("loic", "123"));
-		assertEquals("loic", cas.validateToken(token));
-		// should not throw any exception
-	}
-	
+    
+    private FakeDauphineCas cas;
+    
+    @Before
+    public void setup() {
+        cas = new FakeDauphineCas();
+    }
+    
+    @Test
+    public void testSequence() throws DauphineCasException {
+        String token = cas.authenticate(new Credentials("loic", "123"));
+        assertEquals("loic", cas.validateToken(token));
+        // should not throw any exception
+    }
+    
     @Test(expected = DauphineCasException.class)
-	public void testWrongCredentials() throws DauphineCasException {
-		cas.authenticate(new Credentials("loic", "aaaaaaa"));
-	}
+    public void testWrongCredentials() throws DauphineCasException {
+        cas.authenticate(new Credentials("loic", "aaaaaaa"));
+    }
     
     @Test(expected = DauphineCasException.class)
     public void testWrongToken() throws DauphineCasException {
-	    cas.validateToken("bbbbbb");
+        cas.validateToken("bbbbbb");
     }
     
     @Test
     public void testRoles() throws DauphineCasException {
-        assertArrayEquals(new String[] {Roles.STUDENT}, cas.getRoles("loic"));
+        assertArrayEquals(new String[]{Roles.STUDENT}, cas.getRoles("loic"));
     }
     
     @Test(expected = DauphineCasException.class)
