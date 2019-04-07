81bac1d2fb4f2239109ec2bffe24657df05f3390
omardjalo <omardjalo@yahoo.fr>
2019-04-07 10:03:18 +0200

    Update branche Personne resource

--- dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/PersonResource.java
+++ dauphine-open-data/src/main/java/io/github/oliviercailloux/y2018/opendata/resource/PersonResource.java
@@ -3,7 +3,6 @@ package io.github.oliviercailloux.y2018.opendata.resource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import ezvcard.VCard;
